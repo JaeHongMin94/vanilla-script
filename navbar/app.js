2a1def37894a479ed2722885508f67484daa3559
JaeHongMin94 <moonshin123@naver.com>
2021-01-25 10:25:24 +0900

    navbar

---
+++ navbar/app.js
@@ -0,0 +1,6 @@
+const navbar = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navbar.addEventListener('click', function () {
+  links.classList.toggle('show-links');
+});
