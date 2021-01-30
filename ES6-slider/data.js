f35ba44db8e3a7972a42871c6e391233daac4aaf
JaeHongMin94 <moonshin123@naver.com>
2021-01-30 22:37:47 +0900

    ES6-slider

---
+++ ES6-slider/data.js
@@ -0,0 +1,43 @@
+const people = [
+  {
+    img:
+      'https://blog.kakaocdn.net/dn/bezjux/btqCX8fuOPX/6uq138en4osoKRq9rtbEG0/img.jpg',
+    name: 'Ryan',
+    motive: 'Lion',
+    text:
+      '큰 덩치와 무뚝뚝한 표정으로 오해를 많이 사지만, 사실 누구보다도 여리고 섬세한 소녀감성을 지닌 반전 매력의 소유자! 원래 아프리카 둥둥섬 왕위 계승자였으나, 자유로운 삶을 동경해 탈출!',
+  },
+  {
+    img:
+      'https://file.mk.co.kr/meet/neds/2019/04/image_readtop_2019_209093_15544394573698905.jpg',
+    name: 'Apeach',
+    motive: 'Peach',
+    text:
+      '유전자 변이로 자웅동주가 된 것을 알고 복숭아 나무에서 탈출한 악동 복숭아 어피치! 섹시한 뒷태로 사람들을 매혹시키며, 성격이 매우 급하고 과격합니다.',
+  },
+  {
+    img: 'https://image.yes24.com/momo/TopCate2231/MidCate002/223017924.jpg',
+    name: 'Muzi & Con',
+    motive: 'Radish & Crocodile',
+    text:
+      '호기심 많은 장난꾸러기 무지의 정체는 사실 토끼 옷을 입은 단무지! 토끼 옷을 벗으면 부끄러움을 많이 탑니다. 단무지를 키워 무지를 만든 정체불명의 악어 콘! 이제는 복숭아를 키우고 싶어 어피치를 찾아 다닙니다.',
+  },
+  {
+    img:
+      'https://news.imaeil.com/inc/photos/2015/06/27/2015062705410961639_l.JPG',
+    name: 'Frodo & Neo',
+    motive: 'Dog & Cat',
+    text:
+      '프로도와 네오는 카카오프렌즈 공식 커플로 알콩달콩 깨볶는 중! 부잣집 도시개 프로도는 사실 잡종. 태생에 대한 콤플렉스가 많습니다. 자기 자신을 가장 사랑하는 새침한 고양이 네오.',
+  },
+  {
+    img:
+      'https://i.pinimg.com/originals/57/b2/f4/57b2f4115a4157427377475ff4148525.jpg',
+    name: 'Tube',
+    motive: 'Duck',
+    text:
+      '겁 많고 마음 약한 오리 튜브는 극도의 공포를 느끼면 미친 오리로 변신합니다. 작은 발이 콤플렉스이기 때문에 큰 오리발을 착용합니다. 미운 오리 새끼가 먼 친척입니다.',
+  },
+];
+
+export default people;
